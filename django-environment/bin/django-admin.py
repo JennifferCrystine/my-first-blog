6554d71db00be05068329cd2e061da3f9c6865cd
Jenniffer Crystine <jennifercrystine@id.uff.br>
2019-06-08 11:39:08 -0300

    My Django Girls app, first commit

---
+++ django-environment/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jenniffer/Dropbox/learning-django/django-environment/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
